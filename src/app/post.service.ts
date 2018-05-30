7aca3b8291b3f9be72e4e140c4e1f01e966c1f8e
Robin THOREL <thorelrobin@yahoo.fr>
2018-05-30 20:22:49 +0200

    add method add,delete to PostService

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -4,6 +4,10 @@ import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,29 +17,48 @@ export class PostService {
 
   constructor(private http: HttpClient) { }
 
-    /** GET posts from the server */
-    getPosts(): Observable<Post[]> {
-      return this.http.get<Post[]>(this.postUrl).pipe(
-        tap(students => console.log(`fetched posts`)),
-        catchError(this.handleError('getPosts', []))
-      );
-    }
+  /** GET posts from the server */
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.postUrl).pipe(
+      tap(students => console.log(`fetched posts`)),
+      catchError(this.handleError('getPosts', []))
+    );
+  }
 
-    /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-    private handleError<T>(operation = 'operation', result?: T) {
-      return (error: any): Observable<T> => {
+  /** POST: add a new post to the server */
+  addPost(post: Post): Observable<Post> {
+    return this.http.post<Post>(this.postUrl, post, httpOptions).pipe(
+      tap((post: Post) => console.log(`added hero w/ id=${post.id}`)),
+      catchError(this.handleError<Post>('addHero'))
+    );
+  }
 
-        // TODO: send the error to remote logging infrastructure
-        console.error(error); // log to console instead
+  /** DELETE: delete the post from the server */
+  deletePost(post: Post | number): Observable<Post> {
+    const id = typeof post === 'number' ? post : post.id;
+    const url = `${this.postUrl}/${id}`;
 
-        // Let the app keep running by returning an empty result.
-        return of(result as T);
-      };
-    }
+    return this.http.delete<Post>(url, httpOptions).pipe(
+      tap(_ => console.log(`deleted post id=${id}`)),
+      catchError(this.handleError<Post>('deletePost'))
+    );
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../post.service';
+import { StudentService } from '../student.service';
 import { Post } from '../post';
+import { Student } from '../student';
 
 @Component({
   selector: 'app-post',
@@ -10,16 +12,40 @@ import { Post } from '../post';
 export class PostComponent implements OnInit {
 
   posts: Post[];
+  students: Student[];
 
-  constructor(private postService: PostService) { }
+  constructor(
+    private postService: PostService,
+    private studentService: StudentService
+  ) { }
 
   ngOnInit() {
     this.getPosts();
+    this.getStudents();
   }
 
   getPosts(): void {
     this.postService.getPosts()
         .subscribe(posts => this.posts = posts);
+  }
+
+  getStudents(): void {
+    this.studentService.getStudents()
+        .subscribe(students => this.students = students)
+  }
+
+  add(content: string): void {
+    content = content.trim();
+    if (!content) { return; }
+    this.postService.addPost({ content } as Post)
+      .subscribe(post => {
+        this.posts.push(post);
+      });
+  }
+
+  delete(post: Post): void {
+    this.posts = this.posts.filter(h => h !== post);
+    this.postService.deletePost(post).subscribe();
   }
 
 }
